d53fa5568ab9205da76ef7c37a6d3e53b1b12840
Robert C Upchurch <robertc.upchurch@gmail.com>
2018-12-04 17:39:30 -0700

    Add Sign Up

--- ISIntex/ISIntex/Models/Customer.cs
+++ ISIntex/ISIntex/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +9,42 @@ namespace ISIntex.Models
 {
     public class Customer
     {
+        [Required(ErrorMessage = "This is Required")]
         public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
+        [DisplayName("Company")]
         public string Company { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
+        [DisplayName("Email")]
+        [EmailAddress(ErrorMessage = "This is not a valid email")]
         public string Email { get; set; }
-        public int Phone { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
+        [DisplayName("Phone Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "This is Required")]
         public string Zip { get; set; }
+
         public int SalesRepID { get; set; }
         public string PaymentInfo { get; set; }
     }
