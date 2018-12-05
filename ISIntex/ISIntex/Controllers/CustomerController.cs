89888a828d4ae66c2789a09bbde0cd0a3210a78c
Robert C Upchurch <robertc.upchurch@gmail.com>
2018-12-05 15:30:04 -0700

    Add login Identification

--- ISIntex/ISIntex/Controllers/CustomerController.cs
+++ ISIntex/ISIntex/Controllers/CustomerController.cs
@@ -11,14 +11,30 @@ namespace ISIntex.Controllers
     public class CustomerController : Controller
     {
 
-        private NorthwestContext db = new NorthwestContext(); 
+        private static NorthwestContext db = new NorthwestContext();
+        public static List<Customer> CustomerInfo = db.Customers.ToList();
 
         // GET: Customer
         public ActionResult Index()
         {
             if (Authorized.userAuth == "customer")
             {
-                 
+                foreach (var item in CustomerInfo)
+                {
+                    if (item.Email == Authorized.Email)
+                    {
+                        Authorized.CustomerID = item.CustomerID;
+                        Authorized.FirstName = item.FirstName;
+                        Authorized.LastName = item.LastName;
+                        Authorized.Company = item.Company;
+                        Authorized.Email = item.Email;
+                        Authorized.Phone = item.Phone;
+                        Authorized.Address = item.Address;
+                        Authorized.City = item.City;
+                        Authorized.State = item.State;
+                        Authorized.Zip = item.Zip;
+                    }
+                } 
                 return View(); 
             }
             else
